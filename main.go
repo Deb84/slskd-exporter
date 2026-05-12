ab7c07d95f9487595908a3ee2e2d77da7a11fb78
Deb84 <56758812+Deb84@users.noreply.github.com>
2026-05-12 17:55:15 +0200

    fix(main): fixed scrape error msg duration format

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 
 		err := scrape(db, session)
 		if err != nil {
-			slog.Error(fmt.Sprintf("Unable to complete this scrape, retrying in %ds", env.ScrapeInterval.Seconds()))
+			slog.Error(fmt.Sprintf("Unable to complete this scrape, retrying in %fs", env.ScrapeInterval.Seconds()))
 		}
 		mu.Unlock()
 	}
